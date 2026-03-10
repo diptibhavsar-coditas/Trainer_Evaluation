6b9c7e2065b8fb9f1ab8b9cbd865d80b585fd27a
diptibhavsar-coditas <dipti.bhavsar@coditas.com>
2026-03-10 15:43:09 +0530

    Refactor Employee class to use Lombok for getters/setters
    Replaced manual getters and setters with Lombok annotations for cleaner code.

--- JPAProject/src/main/java/com/example/JPAProject/entity/Employee.java
+++ JPAProject/src/main/java/com/example/JPAProject/entity/Employee.java
@@ -1,9 +1,15 @@
 package com.example.JPAProject.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
 @Entity
+@Getter
+@Setter
+
 public class Employee {
 
     @Id
@@ -16,66 +22,7 @@ public class Employee {
     private String location;
 
     private Double salary;
-
-    private Date hireDate;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getDepartment() {
-		return department;
-	}
-
-	public void setDepartment(String department) {
-		this.department = department;
-	}
-
-	public String getLocation() {
-		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-	public Double getSalary() {
-		return salary;
-	}
-
-	public void setSalary(Double salary) {
-		this.salary = salary;
-	}
-
-	public Date getHireDate() {
-		return hireDate;
-	}
-
-	public void setHireDate(Date hireDate) {
-		this.hireDate = hireDate;
-	}
-
-    // getters and setters
+    private String hireDate;
     
     
 }
