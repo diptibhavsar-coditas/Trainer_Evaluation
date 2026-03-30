e4f7498939dc87b6280ba49d788cc4851fe26049
diptibhavsar-coditas <dipti.bhavsar@coditas.com>
2026-03-30 16:32:24 +0530

    Delete repository directory

--- repository/MedicalHistoryRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.OneToOne.repository;
-
-import com.example.OneToOne.entity.MedicalHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicalHistoryRepo extends JpaRepository<MedicalHistory,Long> {
-}
